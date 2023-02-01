47e756a940493e1733f0412606e3214051a9c209
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-01 13:13:37 +0100

    POST store function first draft done

--- src/controllers/order_controller.ts
+++ src/controllers/order_controller.ts
@@ -25,6 +25,31 @@ export const show = async (req: Request, res: Response) => {
  * Create a order
  */
 export const store = async (req: Request, res: Response) => {
+    const { customer_first_name, customer_last_name, customer_address, customer_postcode, customer_city, customer_email, customer_phone, order_total, order_items } = req.body
+
+    try {
+        const order = await prisma.order.create({
+            data: {
+                customer_first_name,
+                customer_last_name,
+                customer_address,
+                customer_postcode,
+                customer_city,
+                customer_email,
+                customer_phone,
+                order_total,
+                order_items,
+            },
+        })
+
+        res.status(201).send({
+            status: "success",
+            data: order,
+        })
+    }
+    catch (err) {
+        
+    }
 }
 
 /**
