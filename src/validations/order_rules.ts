7fc4ae58cd5ed165c60a14ade74021d547299fe3
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-02 18:17:23 +0100

    order_total validation adjusted, _rules.ts looks nicer

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -10,13 +10,40 @@ const isValidOrderTotal: CustomValidator = async total => {
 }
 
 export const createOrderRules = [
-    body('customer_first_name').isString().withMessage('has to be a string').bail().isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
-    body('customer_last_name').isString().withMessage('has to be a string').bail().isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
-    body('customer_address').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3 - 191 chars'),
-    body('customer_postcode').isString().withMessage('has to be a string').bail().isLength({ max: 6 }).withMessage("can't be more than 6 chars"),
-    body('customer_city').isString().withMessage('has to be a string').bail().isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
-    body('customer_email').isString().withMessage('has to be a string').bail().isEmail().withMessage('has to be a valid email'),
-    body('customer_phone').optional().isString().withMessage('has to be a string'),
-    body('order_total').isInt().withMessage('has to be a number').bail().not().isString().withMessage('has to be a number').not().isArray().withMessage('has to be a number').custom(isValidOrderTotal),
-    body('order_items').isArray().withMessage('has to be an array [] that contains order_items'),
+    body('customer_first_name')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
+
+    body('customer_last_name')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
+    
+    body('customer_address')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 3, max: 191 }).withMessage('has to be 3 - 191 chars'),
+    
+    body('customer_postcode')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ max: 6 }).withMessage("can't be more than 6 chars"),
+    
+    body('customer_city')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 2, max: 191 }).withMessage('has to be 2 - 191 chars'),
+    
+    body('customer_email')
+        .isString().withMessage('has to be a string').bail()
+        .isEmail().withMessage('has to be a valid email'),
+
+    body('customer_phone')
+        .optional()
+        .isString().withMessage('has to be a string'),
+
+    body('order_total')
+        .isInt().withMessage('has to be a number').bail()
+        .not().isString().withMessage('has to be a number')
+        .not().isArray().withMessage('has to be a number').bail()
+        .custom(isValidOrderTotal),
+
+    body('order_items')
+        .isArray().withMessage('has to be an array [] that contains order_items'),
 ]
--- src/validations/product_rules.ts
+++ src/validations/product_rules.ts
@@ -1,12 +1,7 @@
-
 /**
  * Product Validations
  */
 import { body, CustomValidator } from 'express-validator'
-import Debug from 'debug'
-
-const debug = Debug('bortakvall:product_rules')
-
 
 const isValidStockStatus: CustomValidator = async stock_status => {
     if (!(stock_status === 'instock' || stock_status === 'outofstock')) {
@@ -21,12 +16,30 @@ const isValidPrice: CustomValidator = async price => {
 
 }
 
-
 export const createProductRules = [
-    body('name').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3 - 191 chars long'),
-    body('description').isString().withMessage('has to be a string').bail().isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
-    body('price').isInt().withMessage('has to be a number').bail().not().isString().withMessage('has to be a number').not().isArray().withMessage('has to be a number').bail().custom(isValidPrice),
-    body('images').isObject().withMessage('has to be an object in JSON format'),
-    body('stock_status').isString().withMessage('has to be a string').bail().custom(isValidStockStatus),
-    body('stock_quantity').isInt().withMessage('has to be a number').bail().not().isString().withMessage('has to be a number').not().isArray().withMessage('has to be a number').bail(),
+    body('name')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 3, max: 191 }).withMessage('has to be 3 - 191 chars long'),
+    
+    body('description')
+        .isString().withMessage('has to be a string').bail()
+        .isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
+    
+    body('price')
+        .isInt().withMessage('has to be a number').bail()
+        .not().isString().withMessage('has to be a number')
+        .not().isArray().withMessage('has to be a number').bail()
+        .custom(isValidPrice),
+    
+    body('images')
+        .isObject().withMessage('has to be an object in JSON format'),
+    
+    body('stock_status')
+        .isString().withMessage('has to be a string').bail()
+        .custom(isValidStockStatus),
+    
+    body('stock_quantity')
+        .isInt().withMessage('has to be a number').bail()
+        .not().isString().withMessage('has to be a number')
+        .not().isArray().withMessage('has to be a number'),
 ]
