34b14ed138133d9c1d1ac9636eb6585116c70282
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-03 16:05:14 +0100

    reset all migrations

--- prisma/migrations/20230202182447_order_now_containes_dates_and_times/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `order` ADD COLUMN `created_at` DATETIME NULL,
-    ADD COLUMN `order_date` DATETIME NULL,
-    ADD COLUMN `updated_at` DATETIME NULL;
--- prisma/migrations/20230203150356_migration_reset/migration.sql
+++ prisma/migrations/20230203150356_migration_reset/migration.sql
@@ -4,9 +4,10 @@ CREATE TABLE `Product` (
     `name` VARCHAR(191) NOT NULL,
     `description` TEXT NOT NULL,
     `price` INTEGER UNSIGNED NOT NULL,
+    `on_sale` BOOLEAN NOT NULL,
     `images` JSON NOT NULL,
     `stock_status` VARCHAR(191) NOT NULL DEFAULT 'instock',
-    `stock_quantity` INTEGER UNSIGNED NOT NULL,
+    `stock_quantity` INTEGER UNSIGNED NULL DEFAULT 0,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -14,6 +15,7 @@ CREATE TABLE `Product` (
 -- CreateTable
 CREATE TABLE `Order` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_date` VARCHAR(191) NULL,
     `customer_first_name` VARCHAR(191) NOT NULL,
     `customer_last_name` VARCHAR(191) NOT NULL,
     `customer_address` VARCHAR(191) NOT NULL,
@@ -22,20 +24,26 @@ CREATE TABLE `Order` (
     `customer_email` VARCHAR(191) NOT NULL,
     `customer_phone` VARCHAR(191) NULL,
     `order_total` INTEGER UNSIGNED NOT NULL,
+    `created_at` DATETIME NULL,
+    `updated_at` DATETIME NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `OrderItems` (
-    `product_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `OrderItem` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_id` INTEGER UNSIGNED NOT NULL,
+    `product_id` INTEGER UNSIGNED NOT NULL,
     `qty` INTEGER UNSIGNED NOT NULL,
     `item_price` INTEGER UNSIGNED NOT NULL,
     `item_total` INTEGER UNSIGNED NOT NULL,
-    `orderId` INTEGER UNSIGNED NOT NULL,
 
-    PRIMARY KEY (`product_id`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
